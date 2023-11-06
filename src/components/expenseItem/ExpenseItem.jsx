6c6d331c78e517c7b47c033825bc3431c546636d
srinath-3898 <srinath3898@gmail.com>
2023-11-06 21:30:33 +0530

    Styling and using JS in JSX

--- src/components/expenseItem/ExpenseItem.jsx
+++ src/components/expenseItem/ExpenseItem.jsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./ExpenseItem.module.css";
 const ExpenseItem = () => {
+  const expenseDate = new Date();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const locationOfExpenditure = "Hyderabad";
   return (
-    <div>
-      <h1>Expense Items</h1>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+    <div className={styles.container}>
+      <div>
+        <p className="text_small">{expenseDate.toLocaleDateString()}</p>
+      </div>
+      <div className={styles.expense_item_description}>
+        <p className="text_small">{expenseTitle}</p>
+      </div>
+      <div>
+        <p className="text_small">$ {expenseAmount}</p>
+      </div>
+      <div>
+        <p className="text_small">{locationOfExpenditure}</p>
+      </div>
     </div>
   );
 };
